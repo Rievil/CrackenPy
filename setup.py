f94818e0563363b076a3564cf2de278bc79dcbba
Richard Dvořák <48693908+Rievil@users.noreply.github.com>
2024-02-05 12:51:39 +0100

    Forget the version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import Extension, setup, find_packages
 
 setup(
     name='crackpy',
-    version='0.1.13',
+    version='0.1.14',
     packages=find_packages(where="src"),
     install_requires=[
     'torch>=2.0.0',
