7b88e51b3a6a4d50c722fb8e68454ecdadfe5f71
Richard Dvořák <48693908+Rievil@users.noreply.github.com>
2024-10-11 15:23:15 +0200

    Updated version also

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import Extension, setup, find_packages
 
 setup(
     name="crackpy",
-    version="0.1.51",
+    version="0.1.52",
     packages=find_packages(where="src"),
     install_requires=[
         "torch>=2.0.0",
